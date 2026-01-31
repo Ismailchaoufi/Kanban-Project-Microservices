17734edbe0c8e581002c07775d730e04aad76b9a
Ismailchaoufi <147541365+Ismailchaoufi@users.noreply.github.com>
2026-01-31 21:44:59 +0100

    change production url

--- kanban-frontend/src/environments/environment.prod.ts
+++ kanban-frontend/src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://api-gateway-production-4669.up.railway.app/api/v1'
+  apiUrl: ''
 };
+//https://api-gateway-production-4669.up.railway.app/api/v1
